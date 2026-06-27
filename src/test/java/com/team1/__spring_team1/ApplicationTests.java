4d3c1ecc30b0c886d0b23c65abab81d4ff906090
kwang-i-coder <wkk021121@gmail.com>
2026-06-27 12:28:32 +0900

    project first init

---
+++ src/test/java/com/team1/__spring_team1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.__spring_team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
