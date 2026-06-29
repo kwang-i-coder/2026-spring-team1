ef1fd302384670c0777c8cefe76e77801332a54b
Sjaize <sjaiz1717@gmail.com>
2026-06-29 20:55:05 +0900

    feat: add shared base entities

---
+++ src/main/java/com/team1/__spring_team1/domain/project/entity/ProjectStage.java
@@ -0,0 +1,10 @@
+package com.team1.__spring_team1.domain.project.entity;
+
+public enum ProjectStage {
+    MEETING,
+    PLAN,
+    FEATURE_SPEC,
+    SCREEN_SPEC,
+    WIREFRAME,
+    COMPLETED
+}
