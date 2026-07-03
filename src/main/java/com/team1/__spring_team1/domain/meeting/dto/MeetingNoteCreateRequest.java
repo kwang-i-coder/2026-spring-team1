afebddf4c14afa4570d64cd068a4145b79fa8084
najunho04 <junocontacthere@gmail.com>
2026-07-04 00:36:36 +0900

    feat: Meeting DTO 구현 완료

--- src/main/java/com/team1/__spring_team1/domain/meeting/dto/MeetingNoteCreateRequest.java
+++ src/main/java/com/team1/__spring_team1/domain/meeting/dto/MeetingNoteCreateRequest.java
@@ -1,4 +1,16 @@
 package com.team1.__spring_team1.domain.meeting.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class MeetingNoteCreateRequest {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
 }
--- src/main/java/com/team1/__spring_team1/domain/meeting/dto/MeetingNoteCreateResponse.java
+++ src/main/java/com/team1/__spring_team1/domain/meeting/dto/MeetingNoteCreateResponse.java
@@ -1,4 +1,17 @@
 package com.team1.__spring_team1.domain.meeting.dto;
 
+import lombok.Getter;
+
+@Getter
 public class MeetingNoteCreateResponse {
+
+    private final Long meetingNoteId;
+    private final String title;
+    private final String message;
+
+    public MeetingNoteCreateResponse(Long meetingNoteId, String title) {
+        this.meetingNoteId = meetingNoteId;
+        this.title = title;
+        this.message = "meeting note saved";
+    }
 }
