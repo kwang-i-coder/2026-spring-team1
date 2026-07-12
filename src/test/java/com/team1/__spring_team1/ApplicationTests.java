0d87e30367c56cfcb4d339de08308136c22948dd
kwang-i-coder <wkk021121@gmail.com>
2026-07-12 18:36:22 +0900

    fix: H2 인메모리 데이터베이스 테스트 환경 설정

--- src/test/java/com/team1/__spring_team1/ApplicationTests.java
+++ src/test/java/com/team1/__spring_team1/ApplicationTests.java
@@ -3,9 +3,11 @@ package com.team1.__spring_team1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-@ActiveProfiles({"test"})
+@ActiveProfiles({"local", "test"})
 @SpringBootTest
+@Transactional
 class ApplicationTests {
 
 	@Test
