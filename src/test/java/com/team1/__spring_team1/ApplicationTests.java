908a45e42b29941749c697033742ea0b9a477671
kwang-i-coder <wkk021121@gmail.com>
2026-07-12 18:27:09 +0900

    test: H2 인메모리 데이터베이스 테스트 환경 설정

--- src/test/java/com/team1/__spring_team1/ApplicationTests.java
+++ src/test/java/com/team1/__spring_team1/ApplicationTests.java
@@ -2,7 +2,9 @@ package com.team1.__spring_team1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"test"})
 @SpringBootTest
 class ApplicationTests {
 
