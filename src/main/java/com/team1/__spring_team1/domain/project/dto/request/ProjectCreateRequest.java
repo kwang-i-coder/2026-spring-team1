b8411327e4290a0207c71965728ae63a671a6ad5
ha sung jun <hasungjun1030@gmail.com>
2026-07-15 23:32:37 +0900

    fix: 프로젝트 요청값 검증 로직 추가

--- src/main/java/com/team1/__spring_team1/domain/project/dto/request/ProjectCreateRequest.java
+++ src/main/java/com/team1/__spring_team1/domain/project/dto/request/ProjectCreateRequest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public record ProjectCreateRequest(
         @NotBlank(message = "프로젝트 제목은 필수 조건입니다.")
-        @Size(max = 100)
+        @Size(max = 100, message = "프로젝트 제목은 100자 이하여야야 합니다.")
         String title,
         String description,
         String goal,
